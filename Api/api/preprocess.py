dff9acc81a02c32bebf5c6c432384daa2e2ca2a3
Kobiri <kobiris1998@gmail.com>
2022-05-30 20:40:38 +0000

    Added preprocess file.
    This file contains code to preprocess data files that are uploaded.
    This is to enable the model to run a prediction on the data
    as the feature values would have been cleaned and standardized.

---
+++ Api/api/preprocess.py
@@ -0,0 +1,54 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+
+def preprocess(df):
+
+    scaler = StandardScaler()
+    encoder = LabelEncoder()
+
+    df.drop(
+        columns=["user_id", "REGION", "MRG", "TOP_PACK", "ZONE1", "ZONE2"], inplace=True
+    )
+
+    num_cols = [
+        "MONTANT",
+        "FREQUENCE_RECH",
+        "REVENUE",
+        "ARPU_SEGMENT",
+        "FREQUENCE",
+        "DATA_VOLUME",
+        "ON_NET",
+        "ORANGE",
+        "TIGO",
+        "REGULARITY",
+        "FREQ_TOP_PACK",
+    ]
+
+    for feature in num_cols:
+        df[feature].fillna((df[feature].mean()), inplace=True)
+
+    df[num_cols] = scaler.fit_transform(df[num_cols])
+
+    df["TENURE"] = encoder.fit_transform(df["TENURE"])
+
+    return df
+
+
+######  Testing It Out   ######
+# import joblib
+
+# model = joblib.load("./XGBClassifier.pkl")
+
+# test = pd.read_csv("../../../Data/Test.csv")
+
+# data = preprocess(test)
+# print()
+# print()
+# print(data.select_dtypes(include=["int64", "float64"]).describe().T)
+# print()
+# print(data.info())
+
+# data["CHURN"] = model.predict_proba(test)[:, 1]
+# print(data.head())
